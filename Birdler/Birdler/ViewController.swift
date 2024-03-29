1457ffc4bebdc7ed68c8cff0bcf8c9dfdcf21405
EmersonRicardoSF <ricardinhosaia@gmail.com>
2024-03-29 20:39:58 -0300

    Adicionado o projeto no Xcode.

---
+++ Birdler/Birdler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birdler
+//
+//  Created by Emerson Ricardo Saia Filho on 29/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
