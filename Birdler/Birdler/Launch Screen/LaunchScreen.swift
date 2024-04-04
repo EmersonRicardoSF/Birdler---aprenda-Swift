a0d781f3b0ab0dae4bc8c563bb434f7af50f02e1
EmersonRicardoSF <ricardinhosaia@gmail.com>
2024-04-04 18:39:23 -0300

    Pasta de LaunchScreen criada

---
+++ Birdler/Birdler/Launch Screen/LaunchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreen.swift
+//  Birdler
+//
+//  Created by Emerson Ricardo Saia Filho on 04/04/24.
+//
+
+import UIKit
+
+class LaunchScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
