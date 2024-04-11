ca26131bfd409993a596a5a9e2eca510e5b67c28
Vitor Ernane <vitorernane@hotmail.com>
2024-04-10 23:39:43 -0300

    Adicionei um novo asset de cor para utilizarmos o color Picker do xcode para selecionarmos cores direto dentro dele, criei a view e coloquei o gradiente dentro da Label "Birdler"

--- Birdler/Birdler/Register/Screen/RegisterScreen.swift
+++ Birdler/Birdler/Register/Screen/RegisterScreen.swift
@@ -17,6 +17,28 @@ class RegisterScreen: UIView {
     }()
     
     
+    func getGradientLayer(bounds : CGRect) -> CAGradientLayer{
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [UIColor(red: 0.47, green: 0.05, blue: 0.98, alpha: 1).cgColor, UIColor(red: 0.99, green: 0.55, blue: 0.22, alpha: 1).cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        return gradient
+    }
+    
+    
+    func gradientColor(bounds: CGRect, gradientLayer :CAGradientLayer) -> UIColor? {
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return UIColor(patternImage: image!)
+        
+    }
+    
+    
     lazy var loginLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -29,7 +51,8 @@ class RegisterScreen: UIView {
     
     lazy var viewRegister: UIView = {
         
-        let view = UIView()
+        let view = UIView(frame: .init(x: 0, y: 300, width: 393, height: 629))
+        view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .white
         view.layer.cornerRadius = 28
         
@@ -48,15 +71,20 @@ class RegisterScreen: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
+
     
     private func addElements() {
         
         addSubview(backgroundImageView)
         addSubview(loginLabel)
         addSubview(viewRegister)
+        
+        DispatchQueue.main.async {
+            
+            let gradient = self.getGradientLayer(bounds: self.loginLabel.bounds)
+            self.loginLabel.textColor = self.gradientColor(bounds: self.loginLabel.bounds, gradientLayer: gradient)
+            
+        }
         
     }
     
@@ -69,16 +97,15 @@ class RegisterScreen: UIView {
             backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             
-            loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 100),
+            loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 80),
             loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
-            viewRegister.topAnchor.constraint(equalTo: loginLabel.bottomAnchor,constant: 37),
-            viewRegister.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            viewRegister.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-            
-            
+           // viewRegister.topAnchor.constraint(equalTo: loginLabel.bottomAnchor,constant: 37),
+                   
             ])
-                            
+                
+        
+           
     }
 }
 
