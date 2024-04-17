ce9aebfa70bceb5826c6119180580363d795c6e4
NaylaSales <naylacsales@gmail.com>
2024-04-17 18:57:41 +1000

    Criei  a Screen e a VC da News.

---
+++ Birdler/Birdler/Novidades/Controller/NewsVC.swift
@@ -0,0 +1,18 @@
+//
+//  NewsVC.swift
+//  Birdler
+//
+//  Created by Nayla on 17/4/2024.
+//
+
+import UIKit
+
+class NewsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        
+    }
+
+}
---
+++ Birdler/Birdler/Novidades/Screen/NewsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  NewsScreen.swift
+//  Birdler
+//
+//  Created by Nayla on 17/4/2024.
+//
+
+import UIKit
+
+class NewsScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
