31234b15fa53edaa3c8ddf019cb0cf1ab269ad14
NaylaSales <naylacsales@gmail.com>
2024-05-28 19:54:12 +1000

    Alteramos a cor da navigation.

--- Birdler/Birdler/SceneDelegate.swift
+++ Birdler/Birdler/SceneDelegate.swift
@@ -13,6 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let appearanceNav = UINavigationBar.appearance()
+        appearanceNav.tintColor = .white
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let vc: LoginVC = LoginVC()
